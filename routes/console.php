4c07a046ea36a0d61d10eee8ac926872d8872aee
mo24ali <alichere1234@gmail.com>
2026-02-13 11:12:01 +0100

    last updated in messaging branch

--- routes/console.php
+++ routes/console.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Artisan::command('tarik', function () {
+    $this->comment("rajool");
+})->purpose('Display an inspiring quote');
 
-
-
-
-
+Schedule::command('welcome',function(){
+    
+})->everySecond();
